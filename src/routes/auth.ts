de74ebaf5af30e9d7f024c1ed2578b01fa6314d3
n0wfal <nowfaln@celestialsys.con>
2020-09-08 16:45:21 +0530

    Signup module

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,21 +1,19 @@
-import express, { Request, Response, Router } from 'express';
-import { User } from '../models/user'
+import express, { Request, Response, Router, NextFunction } from 'express';
+import { User, UserRequestAttributes } from '../models/user'
 import models from '../models';
 const user = models.User;
 const router: Router = express.Router();
 /* POST users listing. */
-router.get('/signup', async (req: Request, res: Response) => {
+router.get('/signup', async (req: Request, res: Response, next: NextFunction) => {
   try {
-    
+    const userDetails: UserRequestAttributes = req.body;
+    const newUser: User = await user.create(userDetails);
+
     return res.status(200).json({
       message: "Signup succesful"
     })
   } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      error: error.message
-      
-    })
+    next({})
     }
     
 });
